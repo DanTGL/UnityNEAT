2fc0b721ddf94597fb59e9f9b26a8241d9be79bc
DanTGL <33489389+DanTGL@users.noreply.github.com>
2020-11-25 16:01:14 +0100

    First commit

---
+++ Assets/Scripts/NeuralNetwork.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class NeuralNetwork : MonoBehaviour {
+    
+    public class Node {
+        
+        public float bias;
+
+        public List<Connection> inputConnections;
+
+        public Node(float bias) {
+            this.bias = bias;
+        }
+
+        public float GetValue() {
+            float result = bias;
+
+            foreach (Connection conn in inputConnections) {
+                result += conn.weight * conn.inputNode.GetValue();
+            }
+
+            return result;
+        }
+    }
+
+    public class NeuralLayer {
+        public List<Node> nodes;
+    }
+
+    public List<NeuralLayer> layers;
+
+    public struct Connection {
+
+        public bool enabled;
+
+        public float weight;
+        public Node inputNode;
+    }
+
+    void Start() {
+    }
+
+    void Update() {
+        
+    }
+    
+}
